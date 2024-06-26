de73e018b197aea888bb08be8e56fadfa962c5a4
s26842 <s26842@pjwstk.edu.pl>
2024-06-26 20:34:57 +0200

    Add models for CompanyCustomers

--- Revenue Recognition System/Models/Company.cs
+++ Revenue Recognition System/Models/Company.cs
@@ -2,5 +2,10 @@
 
 public class Company
 {
-    
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Adress { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Krs { get; set; } 
 }
